66e71a9704cc702f8232df74ee10a65659d1ccb0
hev <hev@192.168.1.220>
2015-11-08 17:39:23 +0100

    Improved raw2ircodes

--- tools/raw2ircodes.pl
+++ tools/raw2ircodes.pl
@@ -3,34 +3,45 @@
 # H.Vestergaard 2015
 use strict;
 
+
+if ($#ARGV != 0) {
+    print "\nUsage: raw2ircodes.pl <raw filename>\n";
+    exit;
+}
+my $filename=$ARGV[0];
+
 my @bit_frame;
 my @byte_frame = (0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);;
 
-my $dummy = `cat ir_off_raw`;
+my $dummy = `cat $filename`;
 my $n = 1;
 while ( $dummy =~ /(-?\d+)/g ) {
-	my $bitcode = $1;
-	if ( (($bitcode > -500) and ($bitcode < -300)) or (($bitcode > -1400) and ($bitcode < -1200)) ) {
-		if ($bitcode <= -1200) {
-			$bit_frame[$n] = 1; 
-		} else {
-			$bit_frame[$n] = 0;
-		}
-		$n++;
+    my $bitcode = $1;
+    if ( (($bitcode > -500) and ($bitcode < -300)) or (($bitcode > -1400) and ($bitcode < -1200)) ) {
+        if ($bitcode <= -1200) {
+            $bit_frame[$n] = 1; 
+	} else {
+            $bit_frame[$n] = 0;
 	}
+	$n++;
+    }
 }
 my $crc_byte = 0;
 for (my $i=1; $i <= 27; $i++) {
-	if ($bit_frame[$i*8-7] == 1) { $byte_frame[$i] = 1 };
-	if ($bit_frame[$i*8-6] == 1) { $byte_frame[$i] = $byte_frame[$i] | 2 };
-	if ($bit_frame[$i*8-5] == 1) { $byte_frame[$i] = $byte_frame[$i] | 4 };
-	if ($bit_frame[$i*8-4] == 1) { $byte_frame[$i] = $byte_frame[$i] | 8 };
-	if ($bit_frame[$i*8-3] == 1) { $byte_frame[$i] = $byte_frame[$i] | 16 };
-	if ($bit_frame[$i*8-2] == 1) { $byte_frame[$i] = $byte_frame[$i] | 32 };
-	if ($bit_frame[$i*8-1] == 1) { $byte_frame[$i] = $byte_frame[$i] | 64 };
-	if ($bit_frame[$i*8] == 1) { $byte_frame[$i] = $byte_frame[$i] | 128 };
-	if ( ($i >= 9) && ($i <= 26) ) { $crc_byte = $crc_byte + $byte_frame[$i] }; 		# To check correct crc at byte 27
-	printf '%#x ',  $byte_frame[$i];
+    if ($bit_frame[$i*8-7] == 1) { $byte_frame[$i] = 1 };
+    if ($bit_frame[$i*8-6] == 1) { $byte_frame[$i] = $byte_frame[$i] | 2 };
+    if ($bit_frame[$i*8-5] == 1) { $byte_frame[$i] = $byte_frame[$i] | 4 };
+    if ($bit_frame[$i*8-4] == 1) { $byte_frame[$i] = $byte_frame[$i] | 8 };
+    if ($bit_frame[$i*8-3] == 1) { $byte_frame[$i] = $byte_frame[$i] | 16 };
+    if ($bit_frame[$i*8-2] == 1) { $byte_frame[$i] = $byte_frame[$i] | 32 };
+    if ($bit_frame[$i*8-1] == 1) { $byte_frame[$i] = $byte_frame[$i] | 64 };
+    if ($bit_frame[$i*8] == 1) { $byte_frame[$i] = $byte_frame[$i] | 128 };
+    if ( ($i >= 9) && ($i <= 26) ) { $crc_byte = $crc_byte + $byte_frame[$i] }; 		# To check correct crc at byte 27
+    printf '0x%02x ',  $byte_frame[$i];
+}
+print "\n";
+for (my $i=1; $i <= 27; $i++) {
+    printf '%08b ',  $byte_frame[$i];
 }
 
 printf "\n Check the byte representation of the crc code is equal to last byte: %#x", $crc_byte;
